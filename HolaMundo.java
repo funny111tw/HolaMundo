2c689fd30cf9a1f412e11d32f0569e3ffff3f56a
funny111tw <believefunny@gmail.com>
2018-04-26 01:15:34 -0400

    omitimos class

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+/**
+ * Ejemplo HolaMundo
+ * Imprime el texto
+ */
+   public class holamundo {
+    public static void main (string [] args) {
+      // imprime el texto "Hola Mundo"
+     System.out.println("Hola, Mundo!");
+    }
+   }
+
